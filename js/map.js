3a06a857c1cf08740dffc29e2f789e582a1502ff
mikhailsergeevi4 <mikhailsergeevi4@yandex.ru>
2017-07-12 00:16:19 +0500

    update search

--- js/map.js
+++ js/map.js
@@ -2,57 +2,13 @@ function Success() {
 var map;
 var infoWindow = new google.maps.InfoWindow();
 
-// Favorite locations
-var locations = [
-        {
-          title: 'The dam of the City Pond',
-          lat: 56.837688,
-          lng: 60.603895,
-          wiki: 'Плотина Городского пруда на реке Исеть'
-        },
-        {
-          title: 'Museum of the first president of Russia: Yeltsin',
-          lat: 56.843679,
-          lng: 60.591385,
-          wiki: 'Ельцин-центр'
-        },
-        {
-          title: '"Temple on blood". The place where the last Russian emperor was shot.',
-          lat: 56.844364,
-          lng: 60.60894,
-          wiki: 'Храм на Крови (Екатеринбург)'
-        },
-        {
-          title: 'My school. Which I finished in 2006',
-          lat: 56.838368,
-          lng: 60.600015,
-          wiki: 'Гимназия № 9 (Екатеринбург)'
-        },
-        {
-          title: 'Opera and Ballet Theatre',
-          lat: 56.839534,
-          lng: 60.616412,
-          wiki: 'Екатеринбургский театр оперы и балета'
-        },
-        {
-          title: 'Our Goverment',
-          lat: 56.837491,
-          lng: 60.597531,
-          wiki: 'Здание Свердловского городского совета народных депутатов'
-        },
-        {
-          title: 'Central Mall',
-          lat: 56.837526,
-          lng:  60.595846,
-          wiki: 'Банковский переулок (Екатеринбург)'
-        }
-      ];
+
 // ViewModel function
 var ViewModel = function() {
   // Knockout array
   var self = this;
   self.locationList = ko.observableArray([]);
-  self.filteredLocationList = ko.observableArray([]);
+
 
   // Create the google map zoomed in on Ekaterinburg
   self.initMap = function() {
@@ -74,17 +30,18 @@ var ViewModel = function() {
       google.maps.event.addListener(location.marker(), 'click', function() {
         self.locationClick(location);
       });
+
     });
   };
   // Function when click on a location (in list or marker)
   self.locationClick = function(location) {
     // Set the content of the infoWindow
-    var infoContent = '<div class="marker-title">' + location.title() + '</div><div id="pano"></div>'+
-    '<div id=wiki></div>';
+    var infoContent = '<div class="marker-title">' + location.title + '</div><div id="pano"></div>'+
+    '<div id="wiki"></div>';
 
     infoWindow.setContent(infoContent);
     // Center clicked marker
-    map.panTo(new google.maps.LatLng(location.lat(), location.lng()));
+    map.panTo(new google.maps.LatLng(location.lat, location.lng));
     // Open infoWindow
     infoWindow.open(map, location.marker());
     // Animation
@@ -100,12 +57,12 @@ var ViewModel = function() {
       location.marker().setAnimation(null);
     } else {
       location.marker().setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ location.marker().setAnimation(null); }, 750);
+      setTimeout(function(){ location.marker().setAnimation(null); }, 700);
     }
   };
   // Wikipedia API
   self.wikiInfo = function(location) {
-    $('#wiki').text("");
+  //  $('#wiki').text("");
     // var for wiki API request
     var wikiUrl = 'https://ru.wikipedia.org/w/api.php?action=opensearch&search=' + location.marker().wiki +'&format=json';
     // If there no info - talk to user
@@ -121,7 +78,9 @@ var ViewModel = function() {
         articleStr = articleList[i];
         // Add a link to wikipedia
         var url = 'https://ru.wikipedia.org/wiki/'+ articleStr;
-        $('#wiki').append('<a href="'+ url + '"target="_blank">' + 'Open Wikipedia for more info</a>');
+        var html_url = '<div id="wiki"><a href="'+ url + '"target="_blank">' + 'Open Wikipedia for more info</a></div>'
+        //infoWindow.setContent(html_)
+        //$('#wiki').append('<a href="'+ url + '"target="_blank">' + 'Open Wikipedia for more info</a>');
       }
         clearTimeout(wikiRequestTimeout);
     }
@@ -157,41 +116,38 @@ var ViewModel = function() {
     streetViewService.getPanoramaByLocation(location.marker().position, radius, getStreetView);
   };
 
+
+
   // Function to filter locations
-  self.filterLocations = function() {
-    // Set the filtered locations list to an empty array
-    self.filteredLocationList([]);
-    // Get the search string and the length of the original location list
-    var searchString = $('#search-str').val().toLowerCase();
-    // Loop through each location in the locations list
-    for (var i = 0; i < self.locationList().length; i++) {
-      // Get the current location title
-      var locationName = self.locationList()[i].title().toLowerCase();
-      // If the title match the search string,
-      // add the location to the filtered location list
-      if (locationName.indexOf(searchString) > -1)  {
-        self.filteredLocationList.push(self.locationList()[i]);
-        // Set the map property of the marker to the map
-        self.locationList()[i].marker().setMap(map);
-      } else {
-        // Set the map property of the marker to null so it won't be visible
-        self.locationList()[i].marker().setMap(null);
-      }
-    }
-  };
-  // Return full list if click on "reset"
-  self.resetFiltering = function() {
-      for (var i = 0; i < self.locationList().length; i++) {
-        self.locationList()[i].marker().setMap(map);
-      }
-      self.filteredLocationList(self.locationList());
-  };
+  self.filter = ko.observable("");
+
+      this.filteredLocationList = ko.dependentObservable(function() {
+        var filter = this.filter().toLowerCase();
+
+        if (!filter) {
+        // Return self.locationList() the original array;
+        return ko.utils.arrayFilter(self.locationList(), function(location) {
+          location.marker().setVisible(true);
+          return true;
+        });
+        } else {
+          return ko.utils.arrayFilter(this.locationList(), function(location) {
+            if (location.title.toLowerCase().indexOf(filter) >= 0) {
+            return true;
+            } else {
+              location.marker().setVisible(false);
+            return false;
+            }
+          });
+        }
+      }, this);
 
   google.maps.event.addDomListener(window, 'load', function() {
     self.initMap();
     self.buildLocations();
     self.setLocationClickFunctions();
-    self.filteredLocationList(self.locationList());
+    //self.filteredLocationList(self.locationList());
+
   });
 };
 // Starts everything
@@ -201,16 +157,16 @@ ko.applyBindings( new ViewModel() );
 var Place = function(data) {
   // Set all the properties as knockout observables
   var marker;
-  this.title = ko.observable(data.title);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-  this.wiki = ko.observable(data.wiki);
+  this.title = data.title;
+  this.lat = data.lat;
+  this.lng = data.lng;
+  this.wiki = data.wiki;
   // Google Maps Marker for this location
   marker = new google.maps.Marker({
-    position: new google.maps.LatLng(this.lat(), this.lng()),
+    position: new google.maps.LatLng(this.lat, this.lng),
     map: map,
-    title: this.title(),
-    wiki: this.wiki(),
+    title: this.title,
+    wiki: this.wiki,
     animation: google.maps.Animation.DROP
   });
   // Set the marker as a knockout observable
@@ -221,3 +177,21 @@ var Place = function(data) {
 function googleError() {
   window.alert("Sorry it's problem with Google Maps API");
 }
+
+
+
+
+function openDrawer() {
+  var menu = document.querySelector('#menu');
+  var container = document.querySelector('.container-fluid');
+  var drawer = document.querySelector('.nav');
+
+  menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+  });
+  container.addEventListener('click', function() {
+    drawer.classList.remove('open');
+  });
+};
---
+++ js/model.js
@@ -0,0 +1,45 @@
+// Favorite locations
+var locations = [
+        {
+          title: 'The dam of the City Pond',
+          lat: 56.837688,
+          lng: 60.603895,
+          wiki: 'Плотина Городского пруда на реке Исеть'
+        },
+        {
+          title: 'Museum of the first president of Russia: Yeltsin',
+          lat: 56.843679,
+          lng: 60.591385,
+          wiki: 'Ельцин-центр'
+        },
+        {
+          title: '"Temple on blood". The place where the last Russian emperor was shot.',
+          lat: 56.844364,
+          lng: 60.60894,
+          wiki: 'Храм на Крови (Екатеринбург)'
+        },
+        {
+          title: 'My school. Which I finished in 2006',
+          lat: 56.838368,
+          lng: 60.600015,
+          wiki: 'Гимназия № 9 (Екатеринбург)'
+        },
+        {
+          title: 'Opera and Ballet Theatre',
+          lat: 56.839534,
+          lng: 60.616412,
+          wiki: 'Екатеринбургский театр оперы и балета'
+        },
+        {
+          title: 'Our Goverment',
+          lat: 56.837491,
+          lng: 60.597531,
+          wiki: 'Здание Свердловского городского совета народных депутатов'
+        },
+        {
+          title: 'Central Mall',
+          lat: 56.837526,
+          lng:  60.595846,
+          wiki: 'Банковский переулок (Екатеринбург)'
+        }
+      ];
